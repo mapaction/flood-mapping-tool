5891a0195ff9f6d75a11f21a89f9e3e61214b01d
Daniele Castellana <45374794+castledan@users.noreply.github.com>
2022-11-15 18:25:20 +0100

    Revert "Incorporate deployment to streamlit cloud (#4)" (#5)
    This reverts commit 0172b58ac0eb59e49380e9a3f999ba863a3223c7.

--- app/src/utils_ee.py
+++ app/src/utils_ee.py
@@ -1,20 +1,9 @@
 """Module for ee-related functionalities."""
-import os
-
 import ee
 import streamlit as st
-from ee import oauth
-from google.oauth2 import service_account
 
 
-@st.experimental_memo
+@st.cache
 def ee_initialize():
     """Initialise Google Earth Engine."""
-    if "HOSTNAME" in os.environ and os.environ["HOSTNAME"] == "streamlit":
-        service_account_keys = st.secrets["ee_keys"]
-        credentials = service_account.Credentials.from_service_account_info(
-            service_account_keys, scopes=oauth.SCOPES
-        )
-        ee.Initialize(credentials)
-    else:
-        ee.Initialize()
+    ee.Initialize()
