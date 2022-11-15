1139f1fbee05d1b73c5cb47617c3a1055e799f4e
castledan <45374794+castledan@users.noreply.github.com>
2022-11-15 18:34:56 +0100

    let python find out if the app is deployed

--- app/src/utils_ee.py
+++ app/src/utils_ee.py
@@ -1,9 +1,20 @@
 """Module for ee-related functionalities."""
+import os
+
 import ee
 import streamlit as st
+from ee import oauth
+from google.oauth2 import service_account
 
 
-@st.cache
+@st.experimental_memo
 def ee_initialize():
     """Initialise Google Earth Engine."""
-    ee.Initialize()
+    if "HOSTNAME" in os.environ and os.environ["HOSTNAME"] == "streamlit":
+        service_account_keys = st.secrets["ee_keys"]
+        credentials = service_account.Credentials.from_service_account_info(
+            service_account_keys, scopes=oauth.SCOPES
+        )
+        ee.Initialize(credentials)
+    else:
+        ee.Initialize()
