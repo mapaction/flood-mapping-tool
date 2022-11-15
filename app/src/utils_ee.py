326ec1fd53153407326add50f6e662b031477fd2
castledan <45374794+castledan@users.noreply.github.com>
2022-11-15 16:15:32 +0100

    Add new way of authenticating into GEE

--- app/src/utils_ee.py
+++ app/src/utils_ee.py
@@ -1,9 +1,15 @@
 """Module for ee-related functionalities."""
 import ee
 import streamlit as st
+from ee import oauth
+from google.oauth2 import service_account
 
 
 @st.cache
 def ee_initialize():
-    """Initialise Google Earth Engine."""
-    ee.Initialize()
+    """Fetch credentials and initialise Google Earth Engine."""
+    service_account_keys = st.secrets["ee_keys"]
+    credentials = service_account.Credentials.from_service_account_info(
+        service_account_keys, scopes=oauth.SCOPES
+    )
+    ee.Initialize(credentials)
